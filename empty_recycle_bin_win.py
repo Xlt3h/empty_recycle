44ac893363ea7a2ecdcdf14ce949c679a81e36a5
xlt3h <richatdfreeman@gmail.com>
2022-10-23 14:16:08 +0200

    bug fix

--- empty_recycle_bin_win.py
+++ empty_recycle_bin_win.py
@@ -1,12 +1,9 @@
 import winshell as ws
 import ctypes
 
-style = 1 #ok and cancel
-msgbox = ctypes.windll.user32.MessageBoxW(0,"Are you sure you want to clear your recycle bin", "Erase Recycle Bin", style)
 
-if msgbox == 1:
-    try:
-        ws.recycle_bin().empty(sound=True)
-        ctypes.windll.user32.MessageBoxW(0,"Your recycle bin has been erased", "Successful", 0)
-    except:
-        ctypes.windll.user32.MessageBoxW(0,"Cannot Erase your Recycle Bin please restart the application", "Error", 0)
+try:
+    ws.recycle_bin().empty(sound=True)
+    #ctypes.windll.user32.MessageBoxW(0,"Your recycle bin has been erased", "Successful", 0)
+except:
+    ctypes.windll.user32.MessageBoxW(0,"Cannot Erase your Recycle Bin please restart the application", "Error", 0)
